96e31c1d1370b9f3b10230ab41d8b5c9e455444e
anamaric99 <ana.maric@fsre.sum.ba>
2021-09-30 10:15:31 +0200

    prvi commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Dobrodošli na Print shop!';
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        $title = 'O nama';
+        return view('pages.about')->with('title', $title);
+    }
+
+    public function services(){
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Dobrodošli na Print shop!</h1>
+        
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Registracija</a></p>
+    </div>
+@endsection
