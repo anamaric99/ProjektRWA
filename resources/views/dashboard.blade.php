4a06dc557e2f25a9361afe213c3c272ad80344a4
anamaric99 <toni_maric@yahoo.com>
2021-10-11 22:30:42 +0200

    Deveti commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
-                <!-- href="{{ url('/posts/create') }}" -->
+                <!-- href="{{ url('/posts/{{$post->id}}/edit') }}" -->
                 <div class="panel-body">
                     <a href="{{ url('/posts/create') }}" class="btn btn-primary">Create Post</a>
                     <h3>Your Blog Posts</h3>
@@ -21,7 +21,8 @@
                             @foreach($posts as $post)
                                 <tr>
                                     <td>{{$post->title}}</td>
-                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                    <!--  href="/posts/{{$post->id}}/edit -->
+                                    <td><a href="{{ url('/posts/{{$post->id}}/edit') }}" class="btn btn-default">Edit</a></td>
                                     <td>
                                         {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                                             {{Form::hidden('_method', 'DELETE')}}
