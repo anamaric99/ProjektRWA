5202de3002d1b3de0a7979aae26f953fd5253c23
anamaric99 <toni_maric@yahoo.com>
2021-10-11 15:00:29 +0200

    Treci commit

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,5 +2,13 @@
 
 @section('content')
     <h1><?php echo $title; ?></h1>
-    <p>This is the about page</p>
+
+        <div>
+    <img src="ana.jpg" src="C:\wamp64\www\ana\RWA\resources\views\pages\images\ana.jpg" width ="200" height="400" />
+    <p>Zovem se Ana Marić. Dolazim iz Širokog Brijega. Imam 22 godine i studiram računarstvo u Mostaru.</p>
+        </div>
+
+        <img src="renata.jpg" src="C:\wamp64\www\ana\RWA\resources\views\pages\images\renata.jpg" width ="200" height="400" />
+    <p>Zovem se Renata Čule. Dolazim iz Mostara. Imam 21 godinu. Studiram računarstvo u Mostaru.</p>
+
 @endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -4,9 +4,13 @@
     <h1>{{$title}}</h1>
     @if(count($services) > 0)
         <ul class="list-group">
-            @foreach($services as $service)
-                <li class="list-group-item">{{$service}}</li>
-            @endforeach
+            <li> Laravel </li>
+            <li> Wamp server </li>
+            <li> CSS </li>
+
+             <!--  @foreach($services as $service)  
+                <li class="list-group-item">{{$service}}</li>  
+            @endforeach  -->
         </ul>
     @endif
 @endsection
